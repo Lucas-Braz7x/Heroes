839364875a5e568a41c0b65c9b1104d520dce341
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2021-11-25 20:46:54 -0300

    Add React Portal and React-icons

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,8 +3,8 @@ import { Header } from '../components/Layout/Header'
 export const Home = () => {
   return (
     <>
-      <Header></Header>
-      <h1>Noisa</h1>
+      <Header />
+
     </>
   )
 }
